74b0b342f2f08a15907e27b10344a3aa42f7e3cd
mmminboss <lmj284550@gmail.com>
2025-03-20 12:08:41 +0900

    #8 commit!!!

--- src/main/java/com/ezen/matzip/domain/review/controller/ReviewController.java
+++ src/main/java/com/ezen/matzip/domain/review/controller/ReviewController.java
@@ -1,9 +1,17 @@
 package com.ezen.matzip.domain.review.controller;
 
+import com.ezen.matzip.domain.review.service.ReviewService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
+@RequiredArgsConstructor
 public class ReviewController {
+
+    private final ReviewService reviewService;
+
+    public String findReviewByCode()
 }
 
 //review/ → 리뷰 시스템 (리뷰 작성, 수정, 삭제, 조회)
--- src/main/java/com/ezen/matzip/domain/review/service/ReviewService.java
+++ src/main/java/com/ezen/matzip/domain/review/service/ReviewService.java
@@ -1,4 +1,23 @@
 package com.ezen.matzip.domain.review.service;
 
+import com.ezen.matzip.domain.review.dto.ReviewDTO;
+import com.ezen.matzip.domain.review.entity.ReviewEntity;
+import com.ezen.matzip.domain.review.repository.ReviewRepository;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ReviewService {
+
+    private final ReviewRepository reviewRepository;
+    private final ModelMapper modelMapper;
+
+    public ReviewDTO findByRestaurantCode(int restaurantCode){
+
+        ReviewEntity foundReview = reviewRepository.findById(restaurantCode).orElseThrow(IllegalAccessError::new);
+
+        return modelMapper.map(foundReview, ReviewDTO.class);
+    }
 }
