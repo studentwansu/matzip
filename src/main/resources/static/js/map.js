652b6f49500ca14619123145d13f9d0af601d8d5
bbwansu <orm6150@gmail.com>
2025-03-27 12:08:33 +0900

    #29 로그인 시 문구표시 및 로그아웃 작동

---
+++ src/main/resources/static/js/map.js
@@ -0,0 +1,57 @@
+
+let geocoder = new kakao.maps.services.Geocoder();
+
+let callback = function(result, status) {
+    // 정상적으로 검색이 완료됐으면
+    // eslint-disable-next-line no-undef
+    if (status === kakao.maps.services.Status.OK) {
+        // eslint-disable-next-line no-undef
+        const coords = new kakao.maps.LatLng(result[0].y, result[0].x);
+
+        // 결과값으로 받은 위치를 마커로 표시합니다
+        // eslint-disable-next-line no-undef
+        const marker = new kakao.maps.Marker({
+            map: map,
+            position: coords
+        });
+
+        // 지도의 중심을 결과값으로 받은 위치로 이동시킵니다
+        map.setCenter(coords);
+        console.log(marker);
+
+    }
+};
+geocoder.addressSearch(restLoc, callback);
+
+let container = document.getElementById('map');
+let options = {
+    center: new kakao.maps.LatLng(0,0),
+    level: 3
+};
+
+let map = new kakao.maps.Map(container, options);
+
+
+if (navigator.geolocation) {
+
+    // GeoLocation을 이용해서 접속 위치를 얻어옵니다
+    navigator.geolocation.getCurrentPosition(function(position) {
+
+        var lat = position.coords.latitude, // 위도
+            lon = position.coords.longitude; // 경도
+
+        var locPosition = new kakao.maps.LatLng(lat, lon), // 마커가 표시될 위치를 geolocation으로 얻어온 좌표로 생성합니다
+            message = '<div style="padding:5px;">여기에 계신가요?!</div>'; // 인포윈도우에 표시될 내용입니다
+
+        // 마커와 인포윈도우를 표시합니다
+        displayMarker(locPosition, message);
+
+    });
+
+} else { // HTML5의 GeoLocation을 사용할 수 없을때 마커 표시 위치와 인포윈도우 내용을 설정합니다
+
+    var locPosition = new kakao.maps.LatLng(33.450701, 126.570667),
+        message = 'geolocation을 사용할수 없어요..'
+
+    displayMarker(locPosition, message);
+}
---
+++ src/main/resources/static/js/storeedit.js
@@ -0,0 +1,29 @@
+// 메뉴 추가버튼
+const $menu = document.getElementById("menu");
+let num = $menu.querySelectorAll("tr").length;
+
+function addMenu() {
+    let newMenu = $menu.insertRow();
+    let input1 = newMenu.insertCell(0);
+    let input2 = newMenu.insertCell(1);
+    let input3 = newMenu.insertCell(2);
+    let input4 = newMenu.insertCell(3);
+
+    num++;
+
+    // 셀 내부에 직접 <input> 태그 추가
+    input1.innerHTML = `<input type="radio" id="대표메뉴" name="대표메뉴" value="메뉴${num}">`
+    input2.innerHTML = `<input class="store-input-small" type="text" id="메뉴${num}" name="메뉴${num}" placeholder="메뉴명">`;
+    input3.innerHTML = '---------- '
+    input4.innerHTML = `<input class="store-input-small" type="text" id="메뉴${num}가격" name="메뉴${num}가격" placeholder="가격">원`;
+}
+
+// 메뉴 삭제버튼
+
+function deleteMenu() {
+    if (num > 2) {
+        $menu.deleteRow($menu.rows.length - 1);
+        num--;
+    } else
+        alert("더 이상 삭제할 수 없습니다!");
+}
