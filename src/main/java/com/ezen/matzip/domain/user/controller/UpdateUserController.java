0904ccf4da00d8b37acdc7075fcbe0df23bdd412
bbwansu <orm6150@gmail.com>
2025-04-01 00:38:18 +0900

    #40 사업자 회원정보수정페이지 열림

--- src/main/java/com/ezen/matzip/domain/user/controller/UpdateUserController.java
+++ src/main/java/com/ezen/matzip/domain/user/controller/UpdateUserController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.security.Principal;
 
 @Controller
-public class UpdateController {
+public class UpdateUserController {
 
     @Autowired
     private UserUpdateService userUpdateservice;
