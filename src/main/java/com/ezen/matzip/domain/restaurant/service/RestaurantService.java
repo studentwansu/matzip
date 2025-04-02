e5847b98d50b2d02846b2a3ddcd8f5632f02d568
lurufarm <hsg200528@gmail.com>
2025-04-02 12:44:02 +0900

    #44 카테고리로 필터링 완료

--- src/main/java/com/ezen/matzip/domain/restaurant/service/RestaurantService.java
+++ src/main/java/com/ezen/matzip/domain/restaurant/service/RestaurantService.java
@@ -2,11 +2,14 @@ package com.ezen.matzip.domain.restaurant.service;
 
 import com.ezen.matzip.domain.restaurant.dto.RegistDTO;
 import com.ezen.matzip.domain.restaurant.dto.RestaurantDTO;
-import com.ezen.matzip.domain.restaurant.entity.*;
+import com.ezen.matzip.domain.restaurant.entity.Category;
+import com.ezen.matzip.domain.restaurant.entity.Menu;
+import com.ezen.matzip.domain.restaurant.entity.Restaurant;
+import com.ezen.matzip.domain.restaurant.entity.RestaurantKeyword;
 import com.ezen.matzip.domain.restaurant.repository.MenuRepository;
 import com.ezen.matzip.domain.restaurant.repository.RegistRepository;
-import com.ezen.matzip.domain.restaurant.repository.RestaurantRepository;
 import com.ezen.matzip.domain.restaurant.repository.RestaurantKeywordRepository;
+import com.ezen.matzip.domain.restaurant.repository.RestaurantRepository;
 import com.ezen.matzip.domain.review.dto.ReviewDTO;
 import com.ezen.matzip.domain.review.entity.Review;
 import com.ezen.matzip.domain.review.repository.ReviewRepository;
@@ -80,35 +83,34 @@ public class RestaurantService {
 
         Map<RestaurantDTO, Integer> resultSet = new HashMap<>();
 
-        for (Object[] fmenu : foundByMenu)
-        {
-            Restaurant menu = (Restaurant)fmenu[0];
+        for (Object[] fmenu : foundByMenu) {
+            Restaurant rest = (Restaurant) fmenu[0];
             Integer score = ((Number) fmenu[1]).intValue();
 
-            resultSet.put(
-                    new RestaurantDTO(
-                            menu,
-                            menuRepository.findByRestaurantCode(menu),
-                            restaurantKeywordRepository.findByRestaurantCode(menu)
-                    ), score
-            );
+            if (rest.getRestaurantStatus() != 0) {
+                resultSet.put(
+                        new RestaurantDTO(
+                                rest,
+                                menuRepository.findByRestaurantCode(rest),
+                                restaurantKeywordRepository.findByRestaurantCode(rest)
+                        ), score
+                );
+            }
         }
 
-        for (Object[] frest : foundByRestInfo)
-        {
-            Restaurant rest = (Restaurant)frest[0];
+        for (Object[] frest : foundByRestInfo) {
+            Restaurant rest = (Restaurant) frest[0];
             Integer score = ((Number) frest[1]).intValue();
             RestaurantDTO dto = new RestaurantDTO(rest,
                     menuRepository.findByRestaurantCode(rest),
                     restaurantKeywordRepository.findByRestaurantCode(rest));
-
-            if(resultSet.containsKey(dto))
-            {
-                Integer newScore = resultSet.get(dto) + score;
-                resultSet.put(dto, newScore);
+            if (rest.getRestaurantStatus() != 0) {
+                if (resultSet.containsKey(dto)) {
+                    Integer newScore = resultSet.get(dto) + score;
+                    resultSet.put(dto, newScore);
+                } else
+                    resultSet.put(dto, score);
             }
-            else
-                resultSet.put(dto, score);
         }
 
         for (Object[] fkeyw : foundByKeyword)
@@ -118,31 +120,40 @@ public class RestaurantService {
             RestaurantDTO dto = new RestaurantDTO(rest,
                     menuRepository.findByRestaurantCode(rest),
                     restaurantKeywordRepository.findByRestaurantCode(rest));
-
-            if(resultSet.containsKey(dto))
-            {
-                Integer newScore = resultSet.get(dto) + score;
-                resultSet.put(dto, newScore);
+            if(rest.getRestaurantStatus() != 0) {
+                if (resultSet.containsKey(dto)) {
+                    Integer newScore = resultSet.get(dto) + score;
+                    resultSet.put(dto, newScore);
+                } else
+                    resultSet.put(dto, score);
             }
-            else
-                resultSet.put(dto, score);
         }
-
-        resultSet.forEach((key, value) -> {
-            System.out.println("Key: " + key + ", Value: " + value);
-        });
 
         List<Map.Entry<RestaurantDTO, Integer>> sortedList = new ArrayList<>(resultSet.entrySet());
         sortedList.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
         List<RestaurantDTO> finalList = new ArrayList<>();
         for (Map.Entry<RestaurantDTO, Integer> restaurant : sortedList)
-        {
             finalList.add(restaurant.getKey());
-            System.out.println("final: " + restaurant.getKey());
-        }
 
         return finalList;
     }
+
+    public List<RestaurantDTO> filteredRestaurantsByCategory(String keyword, int category)
+    {
+        List<RestaurantDTO> list = findByKeywordOrderByScore(keyword);
+        List<RestaurantDTO> filteredList = new ArrayList<>();
+        for (RestaurantDTO dto : list)
+        {
+            if (dto.getCategoryCode().getCategoryCode() == category)
+            {
+                filteredList.add(dto);
+            }
+        }
+
+        return filteredList;
+    }
+
+//    public List<RestaurantDTO> filteredRestaurantsByCountry(String keyword,)
 
     public String findLocationByRestaurantCode(Integer restaurantCode)
     {
--- src/main/java/com/ezen/matzip/domain/user/repository/UserRepository.java
+++ src/main/java/com/ezen/matzip/domain/user/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.ezen.matzip.domain.user.repository;
 
 import com.ezen.matzip.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByUserId(String userId);
     boolean existsByUserId(String userId);
@@ -16,4 +18,5 @@ public interface UserRepository extends JpaRepository<User, Integer> {
             String passwordQuestion,
             String passwordAnswer
     );
-}
+
+    }
