9c5e63a8a7727ddf38ce357f24c6a8db9984b71b
LeeSang-In <nodt12@gmail.com>
2025-04-02 17:43:31 +0900

    #21 4.02 최종 머지

--- src/main/java/com/ezen/matzip/domain/user/repository/UserRepository.java
+++ src/main/java/com/ezen/matzip/domain/user/repository/UserRepository.java
@@ -4,6 +4,7 @@ import com.ezen.matzip.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
