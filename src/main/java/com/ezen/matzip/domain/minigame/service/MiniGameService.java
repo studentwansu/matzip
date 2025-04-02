990114aa1b1673721c0a10e56c2c683e3740457c
lurufarm <hsg200528@gmail.com>
2025-04-02 16:19:24 +0900

    #45 미니게임 초기 커밋 2

--- src/main/java/com/ezen/matzip/domain/minigame/service/MiniGameService.java
+++ src/main/java/com/ezen/matzip/domain/minigame/service/MiniGameService.java
@@ -1,10 +1,14 @@
 package com.ezen.matzip.domain.minigame.service;
 
 import com.ezen.matzip.domain.weather.dto.KeywordDTO;
+import com.ezen.matzip.domain.weather.entity.Keyword;
 import com.ezen.matzip.domain.weather.repository.KeywordRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -12,9 +16,45 @@ import java.util.List;
 public class MiniGameService {
 
     private final KeywordRepository keywordRepository;
+    private final ModelMapper modelMapper;
 
-    public List<KeywordDTO> sortKeywords(List<KeywordDTO> keywords) {
+    // 1. 처음 키워드 모은 다음 섞기
+    public List<KeywordDTO> shuffleKeywords() {
+
+        List<Keyword> keywords = keywordRepository.findAllByKeywordDescriptionIsNotNull();
+        Collections.shuffle(keywords);
+        List<KeywordDTO> keywordDTOs = new ArrayList<>();
+        for (Keyword keyword : keywords) {
+            keywordDTOs.add(modelMapper.map(keyword, KeywordDTO.class));
+        }
+
+        return keywordDTOs;
+    }
+
+    // 2. 두 개의 키워드 내보내기
+
+    public List<KeywordDTO> showKeywords(List<KeywordDTO> remainedKeywords)
+    {
+        Collections.shuffle(remainedKeywords);
+
+        List<KeywordDTO> showKeywords = new ArrayList<>();
+        showKeywords.add(remainedKeywords.get(0));
+        showKeywords.add(remainedKeywords.get(1));
+
+        return showKeywords;
+    }
+
+
+    public List<KeywordDTO> sortKeyword(String selected, String defeated) {
+
+
         return null;
+    }
+
+    public List<KeywordDTO> miniGame(List<KeywordDTO> remainedKeywords)
+    {
+
+        return remainedKeywords;
     }
 
 }
