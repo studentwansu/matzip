1503a4d48d85481bd571c0e4cfd6802d20094216
LeeSang-In <nodt12@gmail.com>
2025-04-07 17:07:49 +0900

    #21. 4.05 식당 수정 완료

--- src/main/java/com/ezen/matzip/domain/restaurant/service/RestaurantService.java
+++ src/main/java/com/ezen/matzip/domain/restaurant/service/RestaurantService.java
@@ -42,18 +42,25 @@ public class RestaurantService {
     public List<ReviewDTO> getReviewsByRestaurant(int restaurantCode)
     {
         Restaurant restaurant = restaurantRepository.findByRestaurantCode(restaurantCode);
-        List<Review> reviews = reviewRepository.findByRestaurantCode(restaurant);
+        List<Object[]> reviews = reviewRepository.findByRestaurantCode(restaurant);
         List<ReviewDTO> result = new ArrayList<>();
-        for (Review review : reviews)
-        {
+
+
+        for (Object[] review : reviews) {
+            Review e = (Review) review[0];
+            String userId = (String) review[2]; // 🔥 userId도 가져옴
+            String nationality = (String) review[3];
+
             ReviewDTO dto = new ReviewDTO();
             dto.setRestaurantName(restaurant);
             dto.setRestaurantCode(restaurant);
-            dto.setReviewCode(review.getReviewCode());
-            dto.setReviewDate(review.getReviewDate());
-            dto.setReviewContent(review.getReviewContent());
-            dto.setReviewReply(review.getReviewReply());
-            dto.setRating(review.getRating());
+            dto.setReviewCode(e.getReviewCode());
+            dto.setReviewDate(e.getReviewDate());
+            dto.setReviewContent(e.getReviewContent());
+            dto.setReviewReply(e.getReviewReply());
+            dto.setRating(e.getRating());
+            dto.setUserId(userId);
+            dto.setNationality(nationality);
 
             result.add(dto);
         }
