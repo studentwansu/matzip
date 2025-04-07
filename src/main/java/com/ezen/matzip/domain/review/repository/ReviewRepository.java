9f9a6a52ada4c8b19cd6c90e1dbc5400811e06c9
bbwansu <orm6150@gmail.com>
2025-04-07 11:10:32 +0900

    #108 오류수정전 커밋

--- src/main/java/com/ezen/matzip/domain/review/repository/ReviewRepository.java
+++ src/main/java/com/ezen/matzip/domain/review/repository/ReviewRepository.java
@@ -5,6 +5,7 @@ import com.ezen.matzip.domain.review.entity.Review;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -37,4 +38,5 @@ public interface ReviewRepository extends JpaRepository<Review, Integer> {
     @Query("update Review r set r.hiddenFlag = :hiddenFlag where r.userCode = :userCode")
     void updateHiddenFlagByUserCode(@Param("userCode") int userCode, @Param("hiddenFlag") int hiddenFlag);
     //완수 끝
+
 }
