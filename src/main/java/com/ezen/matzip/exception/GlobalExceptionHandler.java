f95d7d0ab39bcec55271ac2193790c0d6c917359
bbwansu <orm6150@gmail.com>
2025-04-07 11:40:50 +0900

    #110 에러페이지 코드 활성화

--- src/main/java/com/ezen/matzip/exception/GlobalExceptionHandler.java
+++ src/main/java/com/ezen/matzip/exception/GlobalExceptionHandler.java
@@ -1,20 +1,20 @@
-//package com.ezen.matzip.exception;
-//
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.ExceptionHandler;
-//import org.springframework.web.bind.annotation.RestControllerAdvice;
-//
-//@RestControllerAdvice
-//public class GlobalExceptionHandler {
-//
-//
-//    //기타 예상치못한 예외처리
-//    @ExceptionHandler(Exception.class)
-//    public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex) {
-//        ErrorResponse errorResponse = new ErrorResponse("INTERNAL_SERVER_ERROR", "예상치 못한 오류가 발생했습니다.");
-//        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
-//    }
-//}
-//
-////GlobalExceptionHandler.java → Spring 전역 예외 처리
+package com.ezen.matzip.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+    //기타 예상치못한 예외처리
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGlobalException(Exception ex) {
+        ErrorResponse errorResponse = new ErrorResponse("INTERNAL_SERVER_ERROR", "예상치 못한 오류가 발생했습니다.");
+        return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
+
+//GlobalExceptionHandler.java → Spring 전역 예외 처리
